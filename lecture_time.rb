ea4614419b2c08d8ce834f343ca79ba77c5400ca
River Pease <riverpease15@gmail.com>
2018-07-13 15:31:20 +0000

    automatically backed up by learn

---
+++ lecture_time.rb
@@ -0,0 +1,12 @@
+class HarryPotterCharacters
+  
+  def initialize(first_name, last_name)
+    @firstname = first_name
+    @lastname = last_name
+    puts "#{@firstname} #{@lastname} is one of my favorite characters!"
+  end
+end
+
+Gryffindor1 =  HarryPotterCharacters.new("Harry", "Potter")
+Slytherin1  = HarryPotterCharacters.new("Draco", "Malfoy")
+Ravenclaw1 = HarryPotterCharacters.new("Luna", "Lovegood")
