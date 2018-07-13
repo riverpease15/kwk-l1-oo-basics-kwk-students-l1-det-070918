0a6de5abe789a4cb774d6effe34314229a4e1f87
River Pease <riverpease15@gmail.com>
2018-07-13 15:59:53 +0000

    automatically backed up by learn

--- good_dog.rb
+++ good_dog.rb
@@ -1,11 +1,20 @@
-def sound
-  
+module Speak
+  def speak(sound)
+    puts "#{sound}"
+  end
+end
 
 class GoodDog
+  include Speak
 end
 
 class HumanBeing
+  include Speak
 end
 
+sparky = GoodDog.new 
+sparky.speak("Arf!")
 
+sara = HumanBeing.new
+sara.speak("Hi!")
 
